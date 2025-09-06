edfe5a19a8297cf056d355849cee1644c2c1d054
Macías Fonseca Alejandro 325724 <amacias15@alumnos.uaq.mx>
2025-09-06 15:59:45 -0600

    refactor(client): renamed host var and added description

--- db_client_alejandroMacias.py
+++ db_client_alejandroMacias.py
@@ -1,6 +1,6 @@
 import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "DS Simple database client")
-    parser.add_argument('--host', metavar = '-H', required=True, type=str, help=
-                        'DB host to connect')
+    parser.add_argument('host', metavar = '-H', required=True, type=str, help=
+                        'DB host to connect (must be valid IP address)')
     args = parser.parse_args()
