f1a6993e4f3ad68e776a73342da99ffde9de1a59
Macías Fonseca Alejandro 325724 <amacias15@alumnos.uaq.mx>
2025-09-06 16:01:51 -0600

    fix(client): changed var to --host again

--- db_client_alejandroMacias.py
+++ db_client_alejandroMacias.py
@@ -12,7 +12,7 @@ def is_valid_ip_address(ip_address: str) -> bool:
     return True
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "DS Simple database client")
-    parser.add_argument('host', metavar = '-H', required=True, type=str, help=
+    parser.add_argument('--host', metavar = '-H', required=True, type=str, help=
                         'DB host to connect (must be valid IP address)')
     args = parser.parse_args()
     if not is_valid_ip_address(args.host):
