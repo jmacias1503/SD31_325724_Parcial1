954506cb34253d4d2fa123383022e7a17c9077da
Macías Fonseca Alejandro 325724 <amacias15@alumnos.uaq.mx>
2025-09-06 19:25:10 -0600

    feat(server): added port argument

--- db_server_alejandroMacias.py
+++ db_server_alejandroMacias.py
@@ -5,7 +5,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "DS Simple Database server")
     parser.add_argument('--address', default='127.0.0.1', nargs='?', type=str, help=
                         'IP address for serving the database (default 127.0.0.1)')
+    parser.add_argument('--port', default=3000, nargs='?', type=int, help=
+                        'Port number to use (default 3000)')
     args = parser.parse_args()
     if not is_valid_ip_address(args.address):
         raise argparse.ArgumentTypeError("Invalid IP address")
     IP_ADDRESS: str = args.address
+    PORT_NUMBER: int = args.port
