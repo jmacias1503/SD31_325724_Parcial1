f4a212b931e7a225dd68e5efd23f777833cbb037
Macías Fonseca Alejandro 325724 <amacias15@alumnos.uaq.mx>
2025-09-06 18:49:15 -0600

    style(server): changed program description

--- db_server_alejandroMacias.py
+++ db_server_alejandroMacias.py
@@ -1,6 +1,6 @@
 import pandas
 import argparse
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description = "Hola desde el server")
+    parser = argparse.ArgumentParser(description = "DS Simple Database server")
     args = parser.parse_args()
     print(args.accumulate(args.integers))
