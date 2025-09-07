955c2668204ad9780cf025fcb9da1e04920291f6
Macías Fonseca Alejandro 325724 <amacias15@alumnos.uaq.mx>
2025-09-06 18:59:23 -0600

    feat(server): added ip address argument

--- db_server_alejandroMacias.py
+++ db_server_alejandroMacias.py
@@ -2,5 +2,7 @@ import pandas
 import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "DS Simple Database server")
+    parser.add_argument('--address', default='127.0.0.1', type=str, help=
+                        'IP address for serving the database (default 127.0.0.1)')
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
+    IP_ADDRESS: str = args.address
